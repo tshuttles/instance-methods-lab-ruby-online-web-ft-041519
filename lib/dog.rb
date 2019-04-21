3e185ca84e49eb7b23a80b81f50941959b4936cb
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-21 02:05:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+
+  dog = Dog.new 
+
+  def bark 
+    puts "Woof!" 
+  end 
+  
+  def sit
+    puts "The Dog is sitting" 
+  end 
+  
+  dog.bark
+  dog.sit 
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person 
+
+  human = Person.new 
+  
+  def talk 
+    puts "Hello World!" 
+  end 
+
+  def walk 
+    puts "The Person is walking"
+  end 
+
+  human.talk 
+  human.walk 
+
+end 
